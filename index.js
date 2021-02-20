0eed551ce5269e7d5099a7a0f4a49b7fbbb88715
codecainer <77582956+codecainer@users.noreply.github.com>
2021-02-20 18:00:30 -0500

    Update index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:3000',
+  clientID: '5tel6IEKkMgzCYtzfi6FHDJkYjfh1ite',
+  issuerBaseURL: 'https://mobileguidesop.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
