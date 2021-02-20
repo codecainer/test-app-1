0a84b1b4e78ad8ba773b3c68a8161e0655f42fee
codecainer <77582956+codecainer@users.noreply.github.com>
2021-02-20 17:44:02 -0500

    initial

---
+++ public/js/routes.js
@@ -0,0 +1,440 @@
+var routes = [
+  // Index page
+  {
+    path: '/index/',
+    url: './index.html',
+    name: 'index',
+  },
+  // home
+  {
+    path: '/home/',
+    url: './home.html',
+    name: 'home',
+  },
+  // list home
+  {
+    path: '/list-home/',
+    url: './list-home.html',
+  },
+  // list elements
+  {
+    path: '/list-elements/',
+    url: './list-elements.html',
+  },
+  // list pages
+  {
+    path: '/list-pages/',
+    url: './list-pages.html',
+  },
+  // list shop
+  {
+    path: '/list-shop/',
+    url: './list-shop.html',
+  },
+  // list apps
+  {
+    path: '/list-apps/',
+    url: './list-apps.html',
+  },
+  // list blog
+  {
+    path: '/list-blog/',
+    url: './list-blog.html',
+  },
+  // main features
+  {
+    path: '/main-features/',
+    url: './main-features.html',
+  },
+  // home default
+  {
+    path: '/home-default/',
+    url: './home-default.html',
+  },
+  // home shop
+  {
+    path: '/home-shop/',
+    url: './home-shop.html',
+  },
+  // home blog
+  {
+    path: '/home-blog/',
+    url: './home-blog.html',
+  },
+  // home news
+  {
+    path: '/home-news/',
+    url: './home-news.html',
+  },
+   // home profile
+  {
+    path: '/home-profile/',
+    url: './home-profile.html',
+  },
+   // home portfolio
+  {
+    path: '/home-portfolio/',
+    url: './home-portfolio.html',
+  },
+   // home splash
+  {
+    path: '/home-splash/',
+    url: './home-splash.html',
+  },
+   // home walkthrough
+  {
+    path: '/home-walkthrough/',
+    url: './home-walkthrough.html',
+  },
+   // home login
+  {
+    path: '/home-login/',
+    url: './home-login.html',
+  },
+
+  // ELements
+
+  // accordion
+  {
+    path: '/accordion/',
+    url: './accordion.html',
+  },
+   // badge
+  {
+    path: '/badge/',
+    url: './badge.html',
+  },
+   // button
+  {
+    path: '/button/',
+    url: './button.html',
+  },
+   // brand-icon
+  {
+    path: '/brand-icon/',
+    url: './brand-icon.html',
+  },
+   // breadcrumbs
+  {
+    path: '/breadcrumbs/',
+    url: './breadcrumbs.html',
+  },
+   // card
+  {
+    path: '/card/',
+    url: './card.html',
+  },
+   // carousel
+  {
+    path: '/carousel/',
+    url: './carousel.html',
+  },
+   // collapse
+  {
+    path: '/collapse/',
+    url: './collapse.html',
+  },
+   // form-elements
+  {
+    path: '/form-elements/',
+    url: './form-elements.html',
+  },
+   // grid
+  {
+    path: '/grid/',
+    url: './grid.html',
+  },
+   // icon
+  {
+    path: '/icon/',
+    url: './icon.html',
+  },
+   // list
+  {
+    path: '/list/',
+    url: './list.html',
+  },
+   // pagination
+  {
+    path: '/pagination/',
+    url: './pagination.html',
+  },
+   // quotes
+  {
+    path: '/quotes/',
+    url: './quotes.html',
+  },
+   // social-media-icon
+  {
+    path: '/social-media-icon/',
+    url: './social-media-icon.html',
+  },
+   // table
+  {
+    path: '/table/',
+    componentUrl: './table.html',
+  },
+   // tabs
+  {
+    path: '/tabs/',
+    url: './tabs.html',
+  },
+   // timeline
+  {
+    path: '/timeline/',
+    url: './timeline.html',
+  },
+  // about
+  {
+    path: '/about/',
+    url: './about.html',
+  },
+  // client
+  {
+    path: '/client/',
+    url: './client.html',
+  },
+  // contact
+  {
+    path: '/contact/',
+    url: './contact.html',
+  },
+  // embeds
+  {
+    path: '/embeds/',
+    url: './embeds.html',
+  },
+  // faq
+  {
+    path: '/faq/',
+    url: './faq.html',
+  },
+  // features
+  {
+    path: '/features/',
+    url: './features.html',
+  },
+  // forum
+  {
+    path: '/forum/',
+    url: './forum.html',
+  },
+  // forum-single
+  {
+    path: '/forum-single/',
+    url: './forum-single.html',
+  },
+  // gallery
+  {
+    path: '/gallery/',
+    url: './gallery.html',
+  },
+  // maintenance
+  {
+    path: '/maintenance/',
+    url: './maintenance.html',
+  },
+  // open-hours
+  {
+    path: '/open-hours/',
+    url: './open-hours.html',
+  },
+  // portfolio
+  {
+    path: '/portfolio/',
+    url: './portfolio.html',
+  },
+  // pricing-table
+  {
+    path: '/pricing-table/',
+    url: './pricing-table.html',
+  },
+  // profile
+  {
+    path: '/profile/',
+    url: './profile.html',
+  },
+  // process
+  {
+    path: '/process/',
+    url: './process.html',
+  },
+  // reservation
+  {
+    path: '/reservation/',
+    url: './reservation.html',
+  },
+  // reset-password
+  {
+    path: '/reset-password/',
+    url: './reset-password.html',
+  },
+  // search-result
+  {
+    path: '/search-result/',
+    url: './search-result.html',
+  },
+  // services
+  {
+    path: '/services/',
+    url: './services.html',
+  },
+  // settings
+  {
+    path: '/settings/',
+    url: './settings.html',
+  },
+  // sign-in
+  {
+    path: '/sign-in/',
+    url: './sign-in.html',
+  },
+  // sign-up
+  {
+    path: '/sign-up/',
+    url: './sign-up.html',
+  },
+  // skill
+  {
+    path: '/skill/',
+    url: './skill.html',
+  },
+  // team
+  {
+    path: '/team/',
+    url: './team.html',
+  },
+  // testimonial
+  {
+    path: '/testimonial/',
+    url: './testimonial.html',
+  },
+  // shop
+  // home
+  {
+    path: '/home-shop/',
+    url: './home-shop.html',
+  },
+  // categories
+  {
+    path: '/categories/',
+    url: './categories.html',
+  },
+   // product
+  {
+    path: '/product/',
+    url: './product.html',
+  },
+  // product-details
+  {
+    path: '/product-details/',
+    url: './product-details.html',
+  },
+  // cart
+  {
+    path: '/cart/',
+    url: './cart.html',
+  },
+  // checkout
+  {
+    path: '/checkout/',
+    url: './checkout.html',
+  },
+  // apps
+  // chat
+  {
+    path: '/chat/',
+    url: './chat.html',
+  },
+  // chat-details
+  {
+    path: '/chat-details/',
+    url: './chat-details.html',
+  },
+  // calendar
+  {
+    path: '/calendar/',
+    url: './calendar.html',
+  },
+  // coming-soon
+  {
+    path: '/coming-soon/',
+    url: './coming-soon.html',
+  },
+  // gallery
+  {
+    path: '/gallery/',
+    url: './gallery.html',
+  },
+  // map
+  {
+    path: '/maps/',
+    url: './maps.html',
+  },
+  // page-not-found
+  {
+    path: '/page-not-found/',
+    url: './page-not-found.html',
+  },
+  // profile
+  {
+    path: '/profile/',
+    url: './profile.html',
+  },
+  // contact
+  {
+    path: '/contact/',
+    url: './contact.html',
+  },
+  // sign-in
+  {
+    path: '/sign-in/',
+    url: './sign-in.html',
+  },
+  // sign-up
+  {
+    path: '/sign-up/',
+    url: './sign-up.html',
+  },
+ // blog
+  {
+    path: '/blog/',
+    url: './blog.html',
+  },
+  // blog-list
+  {
+    path: '/blog-list/',
+    url: './blog-list.html',
+  },
+  // blog-grid
+  {
+    path: '/blog-grid/',
+    url: './blog-grid.html',
+  },
+  // blog-single
+  {
+    path: '/blog-single/',
+    url: './blog-single.html',
+  },
+  // blog-single-gallery
+  {
+    path: '/blog-single-gallery/',
+    url: './blog-single-gallery.html',
+  },
+  // blog-single-video
+  {
+    path: '/blog-single-video/',
+    url: './blog-single-video.html',
+  },
+  // About page
+  {
+    path: '/about/',
+    url: './pages/about.html',
+    name: 'about',
+  },
+  // Default route (404 page). MUST BE THE LAST
+  {
+    path: '(.*)',
+    url: './pages/404.html',
+  },
+];
